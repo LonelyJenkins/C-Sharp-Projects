f52bfbf595f386a0df1de7f0a3166583ae1d9fd1
Roman G <124760225+LonelyJenkins@users.noreply.github.com>
2023-05-23 23:48:18 -0500

    Created Simple Comparison/Math program

---
+++ Math and Comparison Operators/Math and Comparison Operators/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Math_and_Comparison_Operators
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("Person 1");
+            Console.WriteLine("Hourly Rate?");
+            string p1Rate = Console.ReadLine();
+            int p1R = Convert.ToInt32(p1Rate);
+            Console.WriteLine("Hours worked per week?");
+            string p1Hours = Console.ReadLine();
+            int p1H = Convert.ToInt32(p1Hours);
+
+            Console.WriteLine("Person 2");
+            Console.WriteLine("Hourly Rate?");
+            string p2Rate = Console.ReadLine();
+            int p2R = Convert.ToInt32(p2Rate);
+            Console.WriteLine("Hours worked per week?");
+            string p2Hours = Console.ReadLine();
+            int p2H = Convert.ToInt32(p2Hours);
+
+            int p1AnnualSal = (p1R * p1H) * 52;
+            int p2AnnualSal = (p2R * p2H) * 52;
+            bool salary;
+            if (p1AnnualSal > p2AnnualSal)
+            {
+                salary = true;
+            }
+            else
+            {
+                salary = false;
+            }
+
+
+            Console.WriteLine("Annual salary of person 1: $" + p1AnnualSal);
+            Console.WriteLine("Annual salary of person 2: $" + p2AnnualSal);
+
+            Console.WriteLine("Does person 1 make more money than person 2?");
+            Console.WriteLine(salary);
+            Console.ReadLine();
+
+
+        }
+    }
+}
