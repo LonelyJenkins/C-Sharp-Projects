957c202f77162d03251f728bd7a6137e09ac77fd
Roman G <124760225+LonelyJenkins@users.noreply.github.com>
2023-05-24 10:43:18 -0500

    Created Simple Boolean App
    Application consists of simple while and do-while loops

---
+++ Boolean Console App/Boolean Console App/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while loop
+            Console.WriteLine("Guess a number, any number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = false;
+
+            while (!isGuessed)
+            {
+                switch (num)
+                {
+                    case 12:
+                        Console.WriteLine("GREAT! You guessed correctly");
+                        isGuessed = true;
+
+                        //do while loop
+                        int i = 0;
+                        do
+                        {
+                            Console.WriteLine("the answer wasn't " + i + "!");
+                            i++;
+                        }
+                        while (i != 12);
+                        //end do while loop
+                        break;
+
+                    default:
+                        Console.WriteLine("Ouch. You guessed wrong.");
+                        Console.WriteLine("What's your next guess?");
+                        num = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
