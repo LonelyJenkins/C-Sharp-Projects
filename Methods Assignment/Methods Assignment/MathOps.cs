97bf1561b0a33c7e9c22257571ee45c3b087feb2
Roman G <124760225+LonelyJenkins@users.noreply.github.com>
2023-06-06 00:33:51 -0500

    Utilized methods with simple math equations

---
+++ Methods Assignment/Methods Assignment/MathOps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods_Assignment
+{
+    public class MathOps
+    {
+        public static int Multiplication(int num)
+        {
+            int final = num * num;
+            return final;
+        }
+
+        public static int split(int num)
+        {
+            int final = num / 2;
+            return final;
+        }
+
+        public static int cube(int num)
+        {
+            int final = num * num * num;
+            return final;
+        }
+    }
+}
---
+++ Methods Assignment/Methods Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Methods_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, you must type a number that I will then perform maths on...");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int multNum = MathOps.Multiplication(num);
+            int divNum = MathOps.split(num);
+            int cubedNum = MathOps.cube(num);
+            Console.WriteLine("...");
+            Console.ReadLine();
+            Console.WriteLine("Here is your number now squared: " + multNum);
+            Console.ReadLine();
+            Console.WriteLine("Here is your number now split in half: " + divNum);
+            Console.ReadLine();
+            Console.WriteLine("NOW Here is your number cubed: " + cubedNum);
+            Console.ReadLine();
+        }
+    }
+    
+}
