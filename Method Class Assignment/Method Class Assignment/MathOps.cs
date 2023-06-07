3fa5083a46325bdfda1ae5bf309c377dd35dfaa3
Roman G <124760225+LonelyJenkins@users.noreply.github.com>
2023-06-07 01:50:01 -0500

    Utilized void method

---
+++ Method Class Assignment/Method Class Assignment/MathOps.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method_Class_Assignment
+{
+    class MathOps
+    {
+        public void Operation(int num1, int num2)
+        {
+            int final = num1 * num1;
+            Console.WriteLine("The second number you've chosen is: " + num2);
+        }
+    }
+}
---
+++ Method Class Assignment/Method Class Assignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Method_Class_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOps mathOps = new MathOps();
+
+            Console.WriteLine("Enter a number that I may use for maths!");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter another number!!");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            mathOps.Operation(num1: number, num2: number2);
+        }
+    }
+}
