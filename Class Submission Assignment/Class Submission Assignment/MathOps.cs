9f28a45ec585179a777b396da8d9b00307d3f863
Roman G <124760225+LonelyJenkins@users.noreply.github.com>
2023-06-07 02:42:59 -0500

    Uploaded Class Submission Assignment

---
+++ Class Submission Assignment/Class Submission Assignment/MathOps.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Submission_Assignment
+{
+    class MathOps
+    {
+        public void Operation(int num)
+        {
+            int final = num / 2;
+            Console.WriteLine(final);
+        }
+
+        public void Operation(int num, int num1, out int newNum)
+        {
+            int final = num * num1;
+            newNum = final;
+        }
+    }
+}
---
+++ Class Submission Assignment/Class Submission Assignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Class_Submission_Assignment
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            int newNum;
+
+            MathOps mathOps = new MathOps();
+
+            Console.WriteLine("Hello! Please enter a number so I may split it in half!");
+            int num = Convert.ToInt32(Console.ReadLine());
+            mathOps.Operation(num);
+            Console.ReadLine();
+            Console.WriteLine("Enter a new number so we can multiply it!");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            mathOps.Operation(num, num1, out newNum);
+            Console.WriteLine(newNum);
+        }
+    }
+}
