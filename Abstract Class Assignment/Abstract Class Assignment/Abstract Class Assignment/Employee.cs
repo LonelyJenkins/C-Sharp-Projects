dcd411f61f03678cc7c73a443b9642cc0287fb11
Roman G <124760225+LonelyJenkins@users.noreply.github.com>
2023-06-11 15:04:27 -0500

    Added/utilized interface

---
+++ Abstract Class Assignment/Abstract Class Assignment/Abstract Class Assignment/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstract_Class_Assignment
+{
+    class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Hello, " + firstName + " " + lastName);
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("That's it! I quit!!");
+        }
+    }
+}
---
+++ Abstract Class Assignment/Abstract Class Assignment/Abstract Class Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Abstract_Class_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Employee();
+            Console.WriteLine("Enter your first name");
+            person.firstName = Console.ReadLine();
+            Console.WriteLine("Now enter your last name");
+            person.lastName = Console.ReadLine();
+
+            person.SayName();
+            Console.ReadLine();
+
+            IQuittable quit = new Employee();
+            quit.Quit();
+            Console.ReadLine();
+
+        }
+    }
+}
