369db156af82706e685bea9339c7885e4e8371df
Roman G <124760225+LonelyJenkins@users.noreply.github.com>
2023-06-11 13:31:17 -0500

    Created program utilizing abstract class

---
+++ Abstract Class Assignment/Abstract Class Assignment/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstract_Class_Assignment
+{
+    class Employee : Person
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Hello, " + firstName + " " + lastName);
+        }
+    }
+}
---
+++ Abstract Class Assignment/Abstract Class Assignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Abstract_Class_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Employee();
+            Console.WriteLine("Enter your first name");
+            person.firstName = Console.ReadLine();
+            Console.WriteLine("Now enter your last name");
+            person.lastName = Console.ReadLine();
+
+            person.SayName();
+            Console.ReadLine();
+        }
+    }
+}
