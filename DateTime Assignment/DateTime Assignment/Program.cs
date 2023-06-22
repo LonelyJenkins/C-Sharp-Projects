979a27d07b42ebfb4dda0d5fef0fde72bf13ee13
Roman G <124760225+LonelyJenkins@users.noreply.github.com>
2023-06-21 20:44:47 -0500

    Created program utilizing DateTime struct

---
+++ DateTime Assignment/DateTime Assignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DateTime_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("It is currently: " + DateTime.Now);
+            Console.WriteLine("Please choose a number, any number between 1 and 24");
+            int hours  = Convert.ToInt32(Console.ReadLine());
+            DateTime futureTime = DateTime.Now.AddHours(hours);
+            Console.WriteLine("In EXACTLY " + hours + " hours, it will be " + futureTime);
+            Console.ReadLine();
+
+        }
+    }
+}
